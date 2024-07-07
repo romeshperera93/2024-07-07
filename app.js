f9fcfcef04bc2c0c3a3a755db687c2f6c4b76290
romeshperera93 <romeshperera123@gmail.com>
2024-07-07 14:33:46 +0530

    last commit

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
 
 let tblCountries = document.getElementById("tblCountries");
 
+let row = document.getElementById("card-row");
+
+let cardBody ="";
+
 let tableBody = `<tr>
-                    <th>Name</th>
-                    <th>Falg</th>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Falg</th>
                 </tr>` ;
 
 fetch("https://restcountries.com/v3.1/all")
@@ -14,19 +18,62 @@ fetch("https://restcountries.com/v3.1/all")
 
     data.forEach(element => {
 
+        // cardBody+=`<div class="col-6">
+        //                 <div class="card" style="width: 18rem;">
+        //                     <img src="${element.flags.png}" class="card-img-top" alt="...">
+        //                         <div class="card-body">
+        //                           <h5 class="card-title">${element.name.official}</h5>
+        //                           <p class="card-text">${element.flags.alt}</p>
+        //                           <a href="#" class="btn btn-primary">Go somewhere</a>
+        //                         </div>
+        //                       </div>
+        //                 </div>`
+
         tableBody+=`<tr>
                         <td>
+
                         <h1>${element.name.common}</h1> <br>
-                    
-                        Official Name : ${element.name.official}
+                        <p>Official Name : ${element.name.official}</p>
                         <p>Region : ${element.region} </p>
                         <p>Population : ${element.population} </p>
-                        <a class="btn btn-primary" href="${element.maps.googleMaps}"target ="blank">Go To Map</a>
+                        <a class="btn btn-primary" href="${element.maps.googleMaps}" target="">Go To Map</a>
+                        
                         </td>
                         <td><img src="${element.flags.png}" alt=""></td>
                     </tr>`
         console.log(element.currencies);
+
+        // for(let [key,value] in Object.entries(element.currencies)) { 
+        //     console.log(key,element.currencies); 
+        //   } 
     });
+
+    row.innerHTML=cardBody;
 
     tblCountries.innerHTML=tableBody;
 })
+
+
+function searchCountry(){
+    let userInput = document.getElementById("txtInput").value;
+
+    let flagImg = document.getElementById("flagImg");
+    let name = document.getElementById("name");
+    let officialName = document.getElementById("officialName");
+    let region = document.getElementById("region");
+    let population = document.getElementById("population");
+
+    fetch(`https://restcountries.com/v3.1/name/${userInput}`)
+    .then(res=>res.json())
+    .then(data=>{
+        data.forEach(obj=>{
+            console.log(obj);
+            flagImg.src=obj.flags.png
+            name.innerText=obj.name.common
+            officialName.innerText=obj.name.official
+
+            region.innerText=obj.region;
+            population.innerText=obj.population
+        })
+    })
+}
